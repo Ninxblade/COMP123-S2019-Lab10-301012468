e1c4a7145600e1886a48324618b824f4a1385e9a
JasonLi <jasonli321@hotmail.ca>
2019-07-17 22:20:49 -0400

    Add project files.

---
+++ COMP123-S2019-Lab10-301012468/MainForm.Designer.cs
@@ -0,0 +1,67 @@
+﻿namespace COMP123_S2019_Lab10_301012468
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelControl = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // labelControl
+            // 
+            this.labelControl.AutoSize = true;
+            this.labelControl.Location = new System.Drawing.Point(185, 95);
+            this.labelControl.Name = "labelControl";
+            this.labelControl.Size = new System.Drawing.Size(367, 39);
+            this.labelControl.TabIndex = 0;
+            this.labelControl.Text = "Welcome to Main Form";
+            this.labelControl.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(19F, 38F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightGray;
+            this.ClientSize = new System.Drawing.Size(782, 553);
+            this.Controls.Add(this.labelControl);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(7, 7, 7, 7);
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Main Form";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelControl;
+    }
+}
+
